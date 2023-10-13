1a38d9acce73e0cc2bcbfbf0a76432b86ab3e1f4
Umapathy-G <umapathy.133@gmail.com>
2023-10-13 19:49:54 +0530

    calculator file added

---
+++ vars/calculator.groovy
@@ -0,0 +1,11 @@
+def add(a,b){
+	echo "Sum of two number $a and $b is ${x+y}"
+}
+
+def mul(a,b){
+	echo "Multiplication of two number $a and $b is ${a*b}"
+}
+
+def call(){
+	echo "Calculator shared library called"
+}
